9bed279e115be6fbcd9c4c37ebb24d0a59a03356
SKIPPYNUTSHQ\Aaron <Aaron.Kendall@gmail.com>
2024-09-28 22:24:37 -0400

    Initial revision

---
+++ Scripts/Behaviour/EthereumTokenOwnership.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Nethereum.Unity.Behaviours
+{
+    public class EthereumTokenOwnership
+    {        
+        public long TokenId;
+
+        public long TokenBalance;
+    }
+}
