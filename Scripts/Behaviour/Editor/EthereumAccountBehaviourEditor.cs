9fd71d8bf7f12feda632bf728a00cb20f6073aa5
SKIPPYNUTSHQ\Aaron <Aaron.Kendall@gmail.com>
2024-10-17 23:17:37 -0400

    Added colored textbox to custom editor for Ethereum Account Behaviour, showing an updated balance for the Ether held by the account.

--- Scripts/Behaviour/Editor/EthereumAccountBehaviourEditor.cs
+++ Scripts/Behaviour/Editor/EthereumAccountBehaviourEditor.cs
@@ -1,7 +1,10 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEditor;
 
 using Nethereum.Unity.MultiToken;
+using static Cinemachine.CinemachineBlendDefinition;
+using UnityEngine.UIElements;
 
 namespace Nethereum.Unity.Behaviours
 {
@@ -24,10 +27,70 @@ namespace Nethereum.Unity.Behaviours
             EditorGUILayout.LabelField("");
             GUILayout.EndHorizontal();
 
+            GUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Current Ether Balance");
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            var etherBalance = Convert.ToString(_accountBehaviour.CurrentEtherBalance);
+            EditorGUILayout.TextField(etherBalance, GetGUIStyle(_accountBehaviour.CurrentEtherBalance));
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("");
+            GUILayout.EndHorizontal();
+
             if (GUILayout.Button("Refund All Tokens"))
             {
                 _accountBehaviour.RefundAllOwnedTokens();
             }
+        }
+
+        private GUIStyle GetGUIStyle(decimal etherBalance)
+        {
+            int       setFontSize     = 0;
+            Texture2D normalTexture2D = Texture2D.whiteTexture;
+
+            if (etherBalance < 0.1m)
+            {
+                // normalTexture2D = Texture2D.redTexture;
+                setFontSize     = 18;
+            }
+            else if ((etherBalance > 0.1m) && (etherBalance < 0.5m))
+            {                
+                // normalTexture2D = Texture2D.grayTexture;
+                setFontSize     = 14;
+            }                
+            else
+            {
+                // normalTexture2D = Texture2D.whiteTexture;
+                setFontSize     = 12;
+            }                
+
+            var guiStyle = new GUIStyle()
+            {
+                alignment = TextAnchor.MiddleCenter,
+                fontStyle = FontStyle.Bold,
+                fontSize = setFontSize,
+                normal = new GUIStyleState() { background = normalTexture2D },
+                hover = new GUIStyleState() { background = Texture2D.grayTexture },
+                active = new GUIStyleState() { background = Texture2D.blackTexture }                
+            };
+
+            if (etherBalance < 0.1m)
+            {
+                guiStyle.normal.textColor = Color.red;
+            }
+            else if ((etherBalance > 0.1m) && (etherBalance < 0.5m))
+            {
+                guiStyle.normal.textColor = Color.magenta;
+            }
+            else
+            {
+                guiStyle.normal.textColor = Color.black;
+            }
+            
+            return guiStyle;
         }
     }
 }
