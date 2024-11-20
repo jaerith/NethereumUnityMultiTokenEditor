ac115e4ab02e03e1911fee303146f125dc994654
SKIPPYNUTSHQ\Aaron <Aaron.Kendall@gmail.com>
2024-11-20 15:08:58 -0500

    Initial revision

---
+++ Scripts/Behaviour/UI/NumteHudSimpleUI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Nethereum.Unity.Behaviours.UI
+{
+    public class NumteHudSimpleUI : MonoBehaviour
+    {
+        [SerializeField]
+        Text nameText;
+
+        [SerializeField]
+        Text contractText;
+
+        [SerializeField]
+        Text tokenText;
+
+        [SerializeField]
+        Text tokensOwnedText;
+
+        [SerializeField]
+        Text latestTransactionsText;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
